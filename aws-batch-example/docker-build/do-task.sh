b03b9debb605a74af25c06dfcffd1f075430c332
MAHESH VAIDYA <forvaidya@gmail.com>
2025-03-02 18:36:42 +0530

    working ok

--- aws-batch-example/docker-build/do-task.sh
+++ aws-batch-example/docker-build/do-task.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
-echo "Arguments passed to the script: $@"
-echo "default bucket is $BUCKET_NAME"
+
+set -x 
+TSTAMP=$(date +"%Y-%m-%d_%H-%M-%S")
+OUTFILE="out_${TSTAMP}.txt"
+touch $OUTFILE
+pwd
+readlink -f $OUTFILE
+echo Arguments passed to the script: $@ >> /app/${OUTFILE}
+echo ${BUCKET_NAME}
+aws s3 cp /app/${OUTFILE} s3://${BUCKET_NAME}/${OUTFILE}
+
+
+
 
 
 
